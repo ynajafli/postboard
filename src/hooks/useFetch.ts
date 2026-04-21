a5c114a571766efc6339e62aa2f12a820152eb13
Yusif Najafli <ynajafli@outlook.com>
2026-04-21 11:29:44 -0600

    added a custom hook and fetched post titles

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+export function useFetch<T>(url: string): {
+    data: T | null,
+    isLoading: boolean,
+    error: string | null,
+} {
+    const [data, setData] = useState<T | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        let cancelled = false;
+        async function fetchData() {
+            try {
+                setIsLoading(true);
+                const response = await fetch(url);
+                if (response.ok) {
+                    const jsonResponse = await response.json();
+                    if (!cancelled) setData(jsonResponse);
+                } else {
+                    throw new Error('Request failed!');
+                }
+            } catch (error) {
+                if (!cancelled) setError(error instanceof Error? error.message : 'Error');
+            } finally {
+                if (!cancelled) setIsLoading(false);
+            }
+        }
+        fetchData();
+
+        return () => {
+            cancelled = true;
+        }
+
+    }, [url]);
+    
+    return { data, isLoading, error }
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,25 @@
+import { useFetch } from "../hooks/useFetch";
+import type { Post } from "../types";
 
 function HomePage() {
-
+    const { data, isLoading, error } = useFetch<Post[]>("https://jsonplaceholder.typicode.com/posts/");
     return (
-        <h1>Home</h1>
+        <>
+            <h1>Home</h1>
+            {isLoading && <p>loading...</p>}
+
+            {data && (
+                <ul>
+                    {data.map(item => (
+                        <li key={item.id}>{item.title}</li>
+                    ))}
+                </ul>
+            )}
+
+            {error && (
+                <p>{error}</p>
+            )}
+        </>
     );
 }
 
